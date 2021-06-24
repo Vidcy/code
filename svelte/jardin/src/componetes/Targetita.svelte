f9f335ed56da566ef29ace6aea3103b7dbca0a3e
vidcy <vidcy@localhost.localdomain>
2021-06-24 01:21:23 -0500

    first commit

---
+++ svelte/jardin/src/componetes/Targetita.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let title="titulo de la targeta"
+	export let content="esta es una targeta"
+</script>
+<div>
+	<h3>{title}</h3>
+	<p>{content}</p>
+
+	<button>ingresar</button>
+
+</div>
+
+
+
+
+
+
+
+<style>
+
+</style>
---
+++ svelte/jardin/src/views/Home.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Targetita from '../componetes/Targetita.svelte' 
+	let message="Bienvenido"
+</script>
+
+<h1>{message} </h1>
+	<Targetita 
+		title={"videos"}
+		content={"videos,musica,mas"}
+  />
+	<Targetita 
+		title={"noticias"}
+		content={"videos,musica,mas"}
+	/>
+
+<style>
+
+</style>
